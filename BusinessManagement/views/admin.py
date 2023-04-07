7152e287ef00851091a5ce236ad19821d591e9aa
memu1227 <megha.dnj@gmail.com>
2023-04-06 22:06:52 -0400

    adding changes to admin.py part 5?

--- BusinessManagement/views/admin.py
+++ BusinessManagement/views/admin.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
 from sql.db import DB
 import traceback
-import csv
+#import csv
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 
 @admin.route("/import", methods=["GET","POST"])
@@ -54,6 +54,7 @@ def importCSV():
             stream = io.TextIOWrapper(file.stream._file, "UTF8", newline=None)
             # TODO importcsv-2 read the csv file stream as a dict
             filename_dict = csv.DictReader(stream)
+            import csv
             '''
             UCID: mm2836
             Date Implemented: 04/06/23
