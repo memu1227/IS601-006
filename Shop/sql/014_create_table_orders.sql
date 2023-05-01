43ff8145fc334863bc511b35ae279bfc006cbd2e
memu1227 <megha.dnj@gmail.com>
2023-05-01 15:08:46 -0400

    Orders table

---
+++ Shop/sql/014_create_table_orders.sql
@@ -0,0 +1,14 @@
+CREATE TABLE
+    IS601_Orders(
+        id int auto_increment PRIMARY KEY,
+
+        user_id int not null,
+        total_price float not null,
+        address varchar(240) default null COMMENT  'Address of residence', 
+        payment_method varchar (240) default null COMMENT ' Payment type (card,cash,etc)', 
+        money_received float not null, 
+        first_name varchar(240) default null COMMENT "First Name", 
+        last_name varchar(240) default null COMMENT "Last Name",
+        created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )
