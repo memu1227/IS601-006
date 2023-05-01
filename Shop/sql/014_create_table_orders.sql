f9242777419862975f292fabcb4fc332e5a0ec3d
memu1227 <megha.dnj@gmail.com>
2023-05-01 15:28:52 -0400

    adding changes

--- Shop/sql/014_create_table_orders.sql
+++ Shop/sql/014_create_table_orders.sql
@@ -1,13 +1,14 @@
 CREATE TABLE
-    IS601_Orders(
+    IS601_S_Orders(
         id int auto_increment PRIMARY KEY,
-        user_id int not null,
-        total_price float not null,
-        address varchar(240) default null COMMENT  'Address of residence', 
-        payment_method varchar (240) default null COMMENT ' Payment type (card,cash,etc)', 
-        money_received float not null, 
+        FOREIGN KEY (user_id) REFERENCES IS601_Users(id),
         first_name varchar(240) default null COMMENT "First Name", 
         last_name varchar(240) default null COMMENT "Last Name",
+        address varchar(240) default null COMMENT  'Address of Residence', 
+        number_of_items int,
+        total_price int,
+        payment_method varchar (240) default null COMMENT ' Payment type (card,cash,etc)', 
+        money_received float not null,
         created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )
